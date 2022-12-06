9cb7695c10ce06b1839530eba3f8752a54678ef8
Gabriel J. Soto <Gabriel.SotoGonzalez@inl.gov>
2022-12-06 08:22:16 -0700

    muting model print statements, windowed run now in a new subdirectory

--- use_cases/2021_12/run/dispatchers/dereg_no_print.py
+++ use_cases/2021_12/run/dispatchers/dereg_no_print.py
@@ -4,24 +4,14 @@ different storage technologies in the NYISO market.
 """
 from functools import partial
 
-import os
-import sys
 import numpy as np
 import pyomo.environ as pyo
 from pyomo.opt import SolverStatus, TerminationCondition
-try:
-  import _utils as hutils
-except (ModuleNotFoundError, ImportError):
-  sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-  import _utils as hutils
 
 # Global Constants
-TIME_WINDOW = 24
-PICARD_LIMIT = 10
 ECONV_RATE = 0.33
 HEAT_TECHS = ['Hitec_XL_storage', 'Dowtherm_A_storage']
 E_TECHS = ['ETES_storage', 'Li_ion_storage', 'H2_storage',]
-OTHER_COMPS = ['Additional_NPP', 'NPP_unused', 'turbine']
 
 # This is what the NYISO stack looks like:
 #                                            capacity_gw     marginal_cost
@@ -78,97 +68,24 @@ CASE_DICT = {('Reference', 'No_Storage'): {'coeffs': (12.791, 0.025),
 
 
 def dispatch(info, activity_matrix):
-  """
-  Dispatches the components based on user-defined algorithms.
-  The expected return object is a dict of components, mapped to a dict of
-    resources that component uses, mapped to the amount consumed/produced
-    as a numpy array.
-  Note:
-    - Negative values mean the component consumes that resource
-    - Positive values mean the component produces that resource
-    - The activity doesn't necessarily have to be as long as "time", but it usually should be
-  @ In, info, dict, information about the state of the system
-  @ Out, activity, dict, activity of components as described above
-  """
-  time = getattr(activity_matrix, "_times")
-  heron = info['HERON']
-  case = info['HERON']['Case']
-  components = getattr(activity_matrix, "_components")
-  sources = heron['Sources']
-  labels = heron['Case'].get_labels()
-  label_id = (labels['price_struct'], labels['strategy'])
-  resources = sorted(list(hutils.get_all_resources(components))) # list of all active resources
-
-  # rolling window
-  start_index = 0
-  final_index = len(time)
-  # TODO window overlap!  ( )[ ] -> (   [  )   ]
-  while start_index < final_index:
-    end_index = start_index + int(TIME_WINDOW)
-    if end_index > final_index:
-      end_index = final_index
-    if end_index - start_index == 1:
-      # TODO custom error raise for catching in DispatchManager?
-      raise IOError("A rolling window of length 1 was requested, but this causes crashes in pyomo. " +
-                    "Change the length of the rolling window to avoid length 1 histories.")
-    specific_time = time[start_index:end_index]
-    print('DEBUGG starting window {} to {}'.format(start_index, end_index))
-
-    # set initial storage levels
-    initial_levels = {}
-    for comp in components:
-      if comp.get_interaction().is_type('Storage'):
-        if start_index == 0:
-          initial_levels[comp] = comp.get_interaction().get_initial_level(info)
-        else:
-          initial_levels[comp] = subdisp[comp.name]['level'][comp.get_interaction().get_resource()][-1]
-    # allow for converging solution iteratively
-    converged = False
-    conv_counter = 0
-    previous = None
-    while not converged:
-      conv_counter += 1
-      if conv_counter > PICARD_LIMIT:
-        break
-      # dispatch
-      subdisp = dispatch_window(specific_time, start_index,
-                                      case, components, sources, resources,
-                                      initial_levels, info)
-      # do we need a convergence criteria? Check now.
-      if needs_convergence(components):
-        print(f'DEBUGG iteratively solving window, iteration {conv_counter}/{PICARD_LIMIT} ...')
-        converged = check_converged(subdisp, previous, components)
-        previous = subdisp
-      else:
-        converged = True
-    # store result in corresponding part of dispatch
-    for comp in components:
-      for tag in comp.get_tracking_vars():
-        for res, values in subdisp[comp.name][tag].items():
-          activity_matrix.set_activity_vector(comp, res, values, tracker=tag, start_idx=start_index, end_idx=end_index)
-    start_index = end_index
-  return activity_matrix
-
-def dispatch_window(specific_time, start_index,
-                    case, components, sources, resources,
-                    initial_levels, info):
     """
-      Dispatches one part of a rolling window.
-      @ In, time, np.array, value of time to evaluate
-      @ In, time_offset, int, offset of the time index in the greater history
-      @ In, case, HERON Case, Case that this dispatch is part of
-      @ In, components, list, HERON components available to the dispatch
-      @ In, sources, list, HERON source (placeholders) for signals
-      @ In, resources, list, sorted list of all resources in problem
-      @ In, initial_storage, dict, initial storage levels if any
-      @ In, meta, dict, additional variables passed through
-      @ Out, result, dict, results of window dispatch
+    Dispatches the components based on user-defined algorithms.
+    The expected return object is a dict of components, mapped to a dict of
+      resources that component uses, mapped to the amount consumed/produced
+      as a numpy array.
+    Note:
+     - Negative values mean the component consumes that resource
+     - Positive values mean the component produces that resource
+     - The activity doesn't necessarily have to be as long as "time", but it usually should be
+    @ In, info, dict, information about the state of the system
+    @ Out, activity, dict, activity of components as described above
     """
-    result = {}
     heron = info['HERON']
+    case = info['HERON']['Case']
+    components = dict((c.name, c) for c in heron['Components'])
+    sources = heron['Sources']
     labels = heron['Case'].get_labels()
     label_id = (labels['price_struct'], labels['strategy'])
-    components = dict((c.name, c) for c in heron['Components'])
 
     # Grab wind/solar capacity and wind/solar generation
     wind_cap = CASE_DICT[(labels['price_struct'], labels['strategy'])]['windcap']
@@ -184,8 +101,7 @@ def dispatch_window(specific_time, start_index,
 
     # Since ARMA resolution is by hour we hold this constant
     time_dt = 1
-    T = len(specific_time)
-    time_slice = slice(start_index, start_index+T)
+    T = len(load)
     storage_dat = dict(
         (name, {
             'initial_level': float(components[name].get_interaction().get_initial_level(info)),
@@ -200,7 +116,7 @@ def dispatch_window(specific_time, start_index,
     m = pyo.ConcreteModel()
     Ts = np.arange(0, T, dtype=int)
     m.T = pyo.Set(initialize=Ts)
-    m.Load = load[time_slice]
+    m.Load = load
     m.StorageDat = storage_dat
     m.Labels = label_id
 
@@ -226,7 +142,7 @@ def dispatch_window(specific_time, start_index,
         level = pyo.Var(m.T,
                         within=pyo.NonNegativeReals,
                         bounds=(0, cap),
-                        initialize=initial_levels[components[tes]])
+                        initialize=0)
         charge = pyo.Var(m.T,
                          within=pyo.NonPositiveReals,
                          bounds=(-cap/time_dt, 0),
@@ -268,71 +184,47 @@ def dispatch_window(specific_time, start_index,
         # m.pprint()
         raise RuntimeError('Failed solve!')
 
-    # return dict of numpy arrays
-    result = retrieve_solution(m, add_npp)
-    return result
+    # store activity
+    # NOTE for SecM and E_dump we directly include prices here rather than recalculate them in TEAL,
+    #      since we have the load data now and won't have it (as accessible) then.
+    for htech in HEAT_TECHS:
+        for trackr in ['level', 'charge', 'discharge']:
+            activity_matrix.set_activity_vector(
+                components[htech],
+                'heat',
+                np.array(list(getattr(m, f'{htech}_{trackr}')[t].value for t in m.T)),
+                tracker=trackr
+            )
 
+    # 'grid_sales' is electricity sold to grid coming from NPP, Electrical Storage, Thermal Storage.
+    # However, the turbine output already incorporates elec from the NPP and Thermal Storage, so we just
+    # need to gather the electricity from Electrical Storage.
+    grid_sales = np.zeros(len(m.T))
+    for etech in E_TECHS:
+        for trackr in ['level', 'charge', 'discharge']:
+            act = np.array(list(getattr(m, f'{etech}_{trackr}')[t].value for t in m.T))
+            activity_matrix.set_activity_vector(
+                components[etech],
+                'electricity',
+                act,
+                tracker=trackr
+            )
+            # 'charge' is in both 'charge' and 'discharge' so this is a lil' dirty.
+            if 'charge' in trackr:
+                grid_sales += act
 
-def retrieve_solution(m, add_npp):
-  """
-    Extracts solution from Pyomo optimization
-    @ In, m, pyo.ConcreteModel, associated (solved) model
-    @ Out, result, dict, {comp: {activity: {resource: [production]}} e.g. generator[production][steam]
-  """
-  result = {} # {component: {activity_tag: {resource: production}}}
+    activity_matrix.set_activity_vector(components['Additional_NPP'], 'heat', np.ones(len(m.T)) * add_npp)
+    npp_unused_heat = np.array(list(m.NPP_unused[t].value for t in m.T))
+    activity_matrix.set_activity_vector(components['NPP_unused'], 'heat', npp_unused_heat)
+    turbine_elec = np.array(list(m.Turbine[t].value for t in m.T))
+    activity_matrix.set_activity_vector(components['turbine'], 'electricity', turbine_elec)
+    activity_matrix.set_activity_vector(components['turbine'], 'heat', -turbine_elec / ECONV_RATE)
 
-  # store activity
-  # NOTE for SecM and E_dump we directly include prices here rather than recalculate them in TEAL,
-  #      since we have the load data now and won't have it (as accessible) then.
-  for htech in HEAT_TECHS:
-    result[htech] = {}
-    for trackr in ['level', 'charge', 'discharge']:
-      tag_values = {}
-      tag_values['heat'] = np.array(list(getattr(m, f'{htech}_{trackr}')[t].value for t in m.T))
-      result[htech][trackr] = tag_values
+    # We flip the sign because grid_sales should be all positive and grid is consuming.
+    grid_sales += turbine_elec # (npp_unused_heat / ECONV_RATE)
+    activity_matrix.set_activity_vector(components['grid'], 'electricity', -grid_sales)
 
-  # 'grid_sales' is electricity sold to grid coming from NPP, Electrical Storage, Thermal Storage.
-  # However, the turbine output already incorporates elec from the NPP and Thermal Storage, so we just
-  # need to gather the electricity from Electrical Storage.
-  grid_sales = np.zeros(len(m.T))
-  for etech in E_TECHS:
-    result[etech] = {}
-    for trackr in ['level', 'charge', 'discharge']:
-      tag_values = {}
-      act = np.array(list(getattr(m, f'{etech}_{trackr}')[t].value for t in m.T))
-      tag_values['electricity'] = act
-      result[etech][trackr] = tag_values
-      # 'charge' is in both 'charge' and 'discharge' so this is a lil' dirty.
-      if 'charge' in trackr:
-        grid_sales += act
-
-  for comp in OTHER_COMPS:
-    result[comp] = {}
-    result[comp]['production'] = {}
-    if comp == 'Additional_NPP':
-      npp_values = {}
-      npp_values['heat'] = np.ones(len(m.T)) * add_npp
-      result[comp]['production'] = npp_values
-    elif comp == 'NPP_unused':
-      npp_unused_heat = {}
-      npp_unused_heat['heat'] = np.array(list(m.NPP_unused[t].value for t in m.T))
-      result[comp]['production'] = npp_unused_heat
-    elif comp == 'turbine':
-      turbine_elec_out = {}
-      turbine_elec_in = {}
-      turb_val = np.array(list(m.Turbine[t].value for t in m.T))
-      turbine_elec_out['electricity'] = turb_val
-      turbine_elec_in['heat'] = -turb_val / ECONV_RATE
-      result[comp]['production'] = turbine_elec_out
-      result[comp]['production'] = turbine_elec_in
-
-  # We flip the sign because grid_sales should be all positive and grid is consuming.
-  grid_sales += turb_val # (npp_unused_heat / ECONV_RATE)
-  result['grid'] = {}
-  result['grid']['production'] = {}
-  result['grid']['production']['electricity'] = -grid_sales
-
-  return result
+    return activity_matrix
 
 def _objective(m):
     """
@@ -422,45 +314,6 @@ def _conserve_heat(m, t):
         battery += (charge + discharge)
     return sources + battery + sinks == 0
 
-def needs_convergence(components):
-  """
-    Determines whether the current setup needs convergence to solve.
-    @ In, components, list, HERON component list
-    @ Out, needs_convergence, bool, True if iteration is needed
-  """
-  for comp in components:
-    intr = comp.get_interaction()
-    # storages with a prescribed strategy MAY need iteration
-    if intr.is_governed():
-      return True
-  # if we get here, no iteration is needed
-  return False
-
-def check_converged(new, old, components):
-  """
-    Checks convergence of consecutive dispatch solves
-    @ In, new, dict, results of dispatch # TODO should this be the model rather than dict?
-    @ In, old, dict, results of previous dispatch
-    @ In, components, list, HERON component list
-    @ Out, converged, bool, True if convergence is met
-  """
-  tol = 1e-4 # TODO user option
-  if old is None:
-    return False
-  converged = True
-  for comp in components:
-    intr = comp.get_interaction()
-    name = comp.name
-    tracker = comp.get_tracking_vars()[0]
-    if intr.is_governed(): # by "is_governed" we mean "isn't optimized in pyomo"
-      # check activity L2 norm as a differ
-      # TODO this may be specific to storage right now
-      res = intr.get_resource()
-      scale = np.max(old[name][tracker][res])
-      diff = np.linalg.norm(new[name][tracker][res] - old[name][tracker][res]) / (scale if scale != 0 else 1)
-      if diff > tol:
-        converged = False
-  return converged
 
 def debug_pyomo_print(m):
     print('/' + '='*80)
